4c7ff695047fc8fcc84966c3db4e47a9b39c5195
AnesioSousa <anesios98@gmail.com>
2023-11-02 20:59:54 -0300

    Some bugfix and removed console.logs that was used in debugs

--- app/models/apostador.js
+++ app/models/apostador.js
@@ -3,7 +3,7 @@ const sql = require("./db.js");
 /**
  * Construtor
  */
-const Apostador = function(apostador) {
+const Apostador = function (apostador) {
   this.idPessoa = apostador.idPessoa;
   this.saldo = apostador.saldo;
 };
@@ -29,8 +29,8 @@ Apostador.delete = (idApostador, result) => {
       return;
     }
 
-    if(res.affectedRows === 0){
-      result({kind:"not_found"}, null);
+    if (res.affectedRows === 0) {
+      result({ kind: "not_found" }, null);
       return;
     }
 
@@ -40,56 +40,65 @@ Apostador.delete = (idApostador, result) => {
 };
 
 Apostador.findById = (idApostador, result) => {
-  sql.query("SELECT * FROM pessoa INNER JOIN apostador ON (pessoa.idPessoa = apostador.idPessoa) WHERE idApostador = ? ", idApostador, (err, res) => {
+  sql.query(
+    "SELECT * FROM pessoa INNER JOIN apostador ON (pessoa.idPessoa = apostador.idPessoa) WHERE idApostador = ? ",
+    idApostador,
+    (err, res) => {
       if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
-        }
-      
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+
       if (res.length) {
-          console.log("Apostador found", res[0]);
-          result(null, res[0]);
-          return;
+        //console.log("Apostador found", res[0]);
+        result(null, res[0]);
+        return;
       }
 
       // not found Apostador with the id
       result({ kind: "not_found" }, null);
-  });
+    }
+  );
 };
 
 Apostador.getAll = (result) => {
-  sql.query("SELECT * FROM pessoa INNER JOIN apostador ON (pessoa.idPessoa = apostador.idPessoa)", (err, res) =>{
+  sql.query(
+    "SELECT * FROM pessoa INNER JOIN apostador ON (pessoa.idPessoa = apostador.idPessoa)",
+    (err, res) => {
       if (err) {
-          console.log("error: ", err);
-          result(null, err);
-          return;
+        console.log("error: ", err);
+        result(null, err);
+        return;
       }
 
       console.log("Apostadores: ", res);
       result(null, res);
-  });
+    }
+  );
 };
 
-
 Apostador.updateById = (id, pessoa, result) => {
-  sql.query("SET SQL_SAFE_UPDATES=0; UPDATE pessoa INNER JOIN apostador ON pessoa.idPessoa = apostador.idPessoa SET pessoa.nome = ?, SET pessoa.telefone = ?, SET pessoa.dataNascimento = ?, SET pessoa.cpf = ?, WHERE apostador.idApostador = ?; SET SQL_SAFE_UPDATES=1;",
-   [pessoa.nome, pessoa.telefone, pessoa.dataNascimento, pessoa.cpf, id], (err, res) => {
-    if(err) {
-      console.log("error:", err);
-      result(null, err);
-      return;
-  }
-  
-  if(res.affectedRows == 0){
-      // not found Team with the id
-      result({ kind: "not_found" }, null);
-      return;
-  }
+  sql.query(
+    "SET SQL_SAFE_UPDATES=0; UPDATE pessoa INNER JOIN apostador ON pessoa.idPessoa = apostador.idPessoa SET pessoa.nome = ?, SET pessoa.telefone = ?, SET pessoa.dataNascimento = ?, SET pessoa.cpf = ?, WHERE apostador.idApostador = ?; SET SQL_SAFE_UPDATES=1;",
+    [pessoa.nome, pessoa.telefone, pessoa.dataNascimento, pessoa.cpf, id],
+    (err, res) => {
+      if (err) {
+        console.log("error:", err);
+        result(null, err);
+        return;
+      }
 
-  console.log("update apostador: ", {id: id, ...apostador});
-  result(null, { id: id, ...apostador })
-  })
-}
+      if (res.affectedRows == 0) {
+        // not found Team with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("update apostador: ", { id: id, ...apostador });
+      result(null, { id: id, ...apostador });
+    }
+  );
+};
 
 module.exports = Apostador;
